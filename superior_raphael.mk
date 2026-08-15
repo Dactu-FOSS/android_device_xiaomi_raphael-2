9337c15c334d92cdcf6305deed8849cf337221f6
tactus1 <tactus1@proton.me>
2026-08-15 09:35:43 +0000

    raphael: Superior OS bringup
    This is the bringup for and unofficial build of Superior OS

--- superior_raphael.mk
+++ superior_raphael.mk
@@ -8,13 +8,17 @@
 $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Superior stuff.
+$(call inherit-product, vendor/superior/config/common_full_phone.mk)
+
+SUPERIOR_BUILDTYPE := UNOFFICIAL
+TARGET_BOOT_ANIMATION_RES := 1080
+WITH_GAPPS := flase
 
 # Inherit from raphael device
 $(call inherit-product, device/xiaomi/raphael/device.mk)
 
-PRODUCT_NAME := lineage_raphael
+PRODUCT_NAME := superior_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
